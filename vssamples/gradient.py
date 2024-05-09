af7facacf55d66b22d45ae2d778ece1769a85a49
NSQY <34185288+NSQY@users.noreply.github.com>
2024-05-10 00:31:33 +0930

    add radial gradient

--- vssamples/gradient.py
+++ vssamples/gradient.py
@@ -16,3 +16,33 @@ def gradient(height: int = 50) -> vs.VideoNode:
     splice = splice.resize.Bilinear(height=height)
 
     return splice
+
+def radial() -> vs.VideoNode:
+    import numpy as np
+
+    def _process_frame(n: int, f: vs.VideoNode) -> vs.VideoFrame:
+        f1 = f
+        fout = f1.copy()
+        
+        width = f.width
+        height = f.height
+        
+        x = np.linspace(0, width, width)
+        y = np.linspace(0, height, height)
+        
+        xx, yy = np.meshgrid(x, y)
+        center_x, center_y = width // 2, height // 2
+        
+        radius = np.sqrt((xx - center_x) ** 2 + (yy - center_y) ** 2)
+        
+        radius_normalized = radius / np.max(radius)
+        gradient = np.exp(-n * radius_normalized)
+        
+        np.copyto(np.asarray(fout[0]), gradient)
+
+        return fout
+
+    clip = core.std.BlankClip(None, 256, 256, format=vs.GRAYS, color=[0.1], length=48)
+    clip = clip.std.ModifyFrame(clip, _process_frame)
+
+    return clip
